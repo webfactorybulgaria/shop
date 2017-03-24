53d5169ff9d8a7fe15f78fce3213dee09e01f0e6
Konstantin <konstantin@webfactory.bg>
2017-03-24 11:34:14 +0200

    voucher update

--- src/resources/views/public/_summary.blade.php
+++ src/resources/views/public/_summary.blade.php
@@ -3,7 +3,6 @@
     <br />
     <br />
     <div class="col-md-6">Product: </div>
-    @if ($item->reference_id)
     <div class="col-md-6">
             {{ $item->product }}
             @if(!empty($item->readableAttributes))
@@ -14,33 +13,23 @@
             )
             @endif
     </div>
-    @else
-    <div class="col-md-6">
-        Coupon '{{ $item->sku }}'
-    </div>
-    @endif
-
-    @if ($item->reference_id)
     <div class="col-md-6">SKU: </div>
     <div class="col-md-6">{{ $item->sku }}</div>
-        <div class="col-md-6">Price: </div>
-        <div class="col-md-6">{{ $item->currency }} {{ $item->price }}</div>
-        <div class="col-md-6">Tax: </div>
-        <div class="col-md-6">{{ $item->price }}</div>
+    <div class="col-md-6">Price: </div>
+    <div class="col-md-6">{{ $item->currency }} {{ $item->price }}</div>
+    <div class="col-md-6">Tax: </div>
+    <div class="col-md-6">{{ $item->price }}</div>
 
-        <div class="col-md-6">Quantity: </div>
-        @if(!empty($canEdit))
-            <div class="col-md-1">{{ $item->quantity }}</div>
-            <div class="col-md-5">
-                <a href="{{route(config('app.locale') . '.shop.add', [$item->id, 1])}}" class="btn btn-success">Add 1</a>
-                <a href="{{route(config('app.locale') . '.shop.remove', [$item->id, 1])}}" class="btn btn-danger">Remove 1</a>
-                <a href="{{route(config('app.locale') . '.shop.remove', $item->id)}}" class="btn btn-danger">Remove ALL</a>
-            </div>
-        @else
-            <div class="col-md-6">{{ $item->quantity }}</div>
-        @endif
+    <div class="col-md-6">Quantity: </div>
+    @if(!empty($canEdit))
+        <div class="col-md-1">{{ $item->quantity }}</div>
+        <div class="col-md-5">
+            <a href="{{route(config('app.locale') . '.shop.add', [$item->id, 1])}}" class="btn btn-success">Add 1</a>
+            <a href="{{route(config('app.locale') . '.shop.remove', [$item->id, 1])}}" class="btn btn-danger">Remove 1</a>
+            <a href="{{route(config('app.locale') . '.shop.remove', $item->id)}}" class="btn btn-danger">Remove ALL</a>
+        </div>
     @else
-        <div class="col-md-6">Discount: {{ abs($item->price) }} </div>
+        <div class="col-md-6">{{ $item->quantity }}</div>
     @endif
 
 </div>
