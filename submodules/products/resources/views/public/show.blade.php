961fea4b295bc85ab3dd1d9f40a04ddc019dc3f4
Nasko <nasko@webfactory.bg>
2017-03-24 10:46:30 +0200

    attributes

--- submodules/products/resources/views/public/show.blade.php
+++ submodules/products/resources/views/public/show.blade.php
@@ -17,6 +17,7 @@
             <div class="body">{!! $model->present()->body !!}</div>
             <p>Price: $<span class="js-product-price">{{$model->price}}</span></p>
         </div>
+
         <div class="col-sm-6">
             {!! BootForm::open()->action(route($lang.'.products.add', $model->slug))->role('form') !!}
 
@@ -26,7 +27,7 @@
 
                             @if($group->type == 'dropdown' || $group->type == 'radio')
                             <div class="js-attribute-group">
-                                {!! BootForm::select($group->value, 'attribute[]', $group->items)->class('form-control js-attribute') !!}
+                                {!! BootForm::select($group->value, 'product_attribute['.$group->value.']', $group->items)->class('form-control js-attribute') !!}
                             </div>
                             @elseif($group->type == 'colorbox')
                                 Available colors:
@@ -34,7 +35,7 @@
                                 @foreach($group->items as $key => $color)
                                 <label for="{{$group->value . $key}}">
                                     <div class="col-sm-1">
-                                        {!! BootForm::radio('', 'attribute[]', $key)->id($group->value . $key)->class('js-attribute') !!}
+                                        {!! BootForm::radio('', 'product_attribute['.$group->value.']', $key)->id($group->value . $key)->class('js-attribute') !!}
                                     </div>
                                     <div class="col-sm-6">
                                         <div style="border:1px solid gray;width:50px;height:30px;background-color:{{$color}}"></div>
@@ -47,10 +48,10 @@
                     @endforeach
                     <div class="row">
                         <div class="col-sm-6">
-                        {!! BootForm::text('Custom 1', 'custom[dimension1]') !!}
+                        {!! BootForm::text('Custom 1', 'product_custom_attribute[dimension1]') !!}
                         </div>
                         <div class="col-sm-6">
-                        {!! BootForm::text('Custom 2', 'custom[dimension2]') !!}
+                        {!! BootForm::text('Custom 2', 'product_custom_attribute[dimension2]') !!}
                         </div>
                     </div>
                 @endif
@@ -59,46 +60,5 @@
             {!! BootForm::close() !!}
         </div>
     </article>
-    <!-- <div onload="alert('asd')" ng-app="promocode" ng-controller="PromocodeController">
-        <div ng-show="!promocode">
-            Have a promocode?
-
-            <form name="promoForm">
-                <input type="text" name="coupon" id="coupon" class="form-control" ng-model="form.coupon" required>
-
-                <div class="btn-toolbar">
-                    <div class="btn-primary btn" id="apply-promo" ng-click="checkCoupon()" name="applypromo">@lang('validation.attributes.apply promo')</div>
-                </div>
-            </form>
-        </div>
-        <div ng-show="promocode">
-            <p>Promocode @{{promocode.code}} applied.</p>
-            <p ng-show="promocode.discount > 0">Discount: @{{promocode.discount}}%</p>
-            <p ng-show="promocode.value > 0">Discount: @{{promocode.value}} currency</p>
-        </div>
-    </div> -->
-
-    <div>
-        <div class="ask-for-promo">
-            <span>Have a promocode?</span>
-            <form name="promoForm" id="promo-form">
-                <input type="text" name="coupon" id="coupon" class="form-control" required>
-                <div class="btn-toolbar">
-                    <div class="btn-primary btn" id="apply-promo" name="applypromo">@lang('validation.attributes.apply promo')</div>
-                </div>
-            </form>
-        </div>
-        <div class="applied-promocode hidden">
-            <p>Promocode '<span class="code"></span>' applied.</p>
-            <p class="promocode-discount">Discount: <span class="promo-discount-value"></span>%</p>
-            <p class="promocode-value">Discount: <span class="promo-value"></span> currency</p>
-            <div class="btn-toolbar">
-                <div class="btn-primary btn" id="remove-promo" name="removepromo">Remove promo code</div>
-            </div>
-        </div>
-        <div class="promo-invalid alert alert-danger hidden">
-            Your promo code is expired or invalid.
-        </div>
-    </div>
 
 @endsection
